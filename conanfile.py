3249f2763e4800bb5664343ef707bfcf534ef9e3
Jerry W <jerrywiltse@gmail.com>
2017-08-01 14:28:29 -0400

    updated requires

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,32 @@ class BoostBimapConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/bimap"
-    description = "For a description of this library, please visit http://boost.org/bimap "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "bimap"
+    requires =  "Boost.Concept_Check/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Exception/1.64.0@bincrafters/testing", \
+                      "Boost.Functional/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Lambda/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Multi_Index/1.64.0@bincrafters/testing", \
+                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
+                      "Boost.Property_Map/1.64.0@bincrafters/testing", \
+                      "Boost.Serialization/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Utility/1.64.0@bincrafters/testing"
 
+                      #concept_check5 config0 core2 functional5 iterator5 lambda6 mpl5 multi_index12 preprocessor0 property_map14 serialization11 static_assert1 throw_exception2 type_traits3 utility5
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)

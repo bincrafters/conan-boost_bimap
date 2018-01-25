9f8c08aa35ff4075eacb07d53d3fca053f2c9270
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:54 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostBimapConan(ConanFile):
     name = "boost_bimap"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_bimap"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["bimap"]
     is_in_cycle_group = True
     is_header_only = True

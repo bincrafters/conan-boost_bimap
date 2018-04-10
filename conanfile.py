e7e6bc1f34a1ae9eca7461254abbc7d3c96467c8
Rene Rivera <grafikrobot@gmail.com>
2018-04-10 09:47:10 -0500

    Initial 1.67.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,7 @@ from conans import ConanFile, tools
 
 class BoostBimapConan(ConanFile):
     name = "boost_bimap"
-    version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost_bimap"
+    version = "1.67.0"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
     lib_short_names = ["bimap"]
@@ -18,8 +17,8 @@ class BoostBimapConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_level14group/1.66.0@bincrafters/testing"
+        "boost_level14group/1.67.0@bincrafters/testing",
+        "boost_package_tools/1.67.0@bincrafters/testing"
     )
 
     def package_id_additional(self):
@@ -30,10 +29,11 @@ class BoostBimapConan(ConanFile):
 
     # BEGIN
 
-    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
+    url = "https://github.com/bincrafters/conan-boost_bimap"
+    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.67.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
@@ -61,7 +61,5 @@ class BoostBimapConan(ConanFile):
             import boost_package_tools  # pylint: disable=F0401
             boost_package_tools.package_info(self)
         getattr(self, "package_info_additional", lambda:None)()
-
-
 
     # END

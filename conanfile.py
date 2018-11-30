435d212ae2f64afe21f3350d69398c53a0c9f38a
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 21:59:07 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,12 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
-class BoostTemplateConan(base.BoostBaseConan):
+class BoostBimapConan(base.BoostBaseConan):
     name = "boost_bimap"
     url = "https://github.com/bincrafters/conan-bimap"
     lib_short_names = ["bimap"]
     header_only_libs = ["bimap"]
-    b2_requires = [
-        "boost_level14group",
-    ]
-    
+    cycle_group = "boost_cycle_group_d"
+    b2_requires = ["boost_cycle_group_d"]

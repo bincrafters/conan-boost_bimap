08749fc158052b29c59d8a48b62b6d4481f15fd5
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:28 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostBimapConan(base.BoostBaseConan):
     name = "boost_bimap"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-bimap"
     lib_short_names = ["bimap"]
     header_only_libs = ["bimap"]
